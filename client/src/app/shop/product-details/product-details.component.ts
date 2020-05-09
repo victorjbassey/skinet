b1838486795b3e605fca35417b652b90c8392479
Victor Bassey <viktorbasi545@gmail.com>
2020-05-09 22:53:38 +0100

    Improve UI/UX

--- client/src/app/shop/product-details/product-details.component.ts
+++ client/src/app/shop/product-details/product-details.component.ts
@@ -2,27 +2,39 @@ import { Component, OnInit } from '@angular/core';
 import { IProduct } from 'src/app/shared/models/product';
 import { ShopService } from '../shop.service';
 import { ActivatedRoute } from '@angular/router';
+import { BreadcrumbService } from 'xng-breadcrumb';
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
-  styleUrls: ['./product-details.component.scss']
+  styleUrls: ['./product-details.component.scss'],
 })
 export class ProductDetailsComponent implements OnInit {
   product: IProduct;
 
-  constructor(private shopService: ShopService, private activateRoute: ActivatedRoute) { }
+  constructor(
+    private shopService: ShopService,
+    private activateRoute: ActivatedRoute,
+    private bcService: BreadcrumbService
+  ) {
+    this.bcService.set('@productDetails', '');
+  }
 
   ngOnInit() {
     this.loadProduct();
   }
 
   loadProduct() {
-    this.shopService.getProduct(+this.activateRoute.snapshot.paramMap.get('id')).subscribe(product => {
-      this.product = product;
-    }, error => {
-      console.log(error);
-    });
+    this.shopService
+      .getProduct(+this.activateRoute.snapshot.paramMap.get('id'))
+      .subscribe(
+        (product) => {
+          this.product = product;
+          this.bcService.set('@productDetails', product.name);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
   }
-
 }
